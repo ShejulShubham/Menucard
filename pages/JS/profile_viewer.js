6d4ff28733e13d5c8d4e84b4bda43b98c69af124
Arpita <arpitaambalkar26@gmail.com>
2026-01-05 14:24:56 +0530

    profile viewer

--- pages/JS/profile_viewer.js
+++ pages/JS/profile_viewer.js
@@ -2,7 +2,7 @@
 
 const userLoggedKey="loggedInUser";
 localStorage.setItem(userLoggedKey,JSON.stringify(obj));
-let loggedInUser=Array(JSON.parse(localStorage.getItem(userLoggedKey))) || [];
+let loggedInUser=JSON.parse(localStorage.getItem(userLoggedKey)) || [];
 
 let container=document.getElementById("container");
 let msg=document.getElementById("msg");
@@ -16,14 +16,14 @@ function renderloggedUser()
     }
     let card=document.createElement("div");
     card.innerHTML="";
-    loggedInUser.forEach((item)=>{
+    
         card.innerHTML=`
-        <label>Name : <input id="name" value=${item.name} disabled></label>
-            <label>Email : <input id="email" value=${item.email} disabled></label>
-            <label>Password : <input type="password" id="password" disabled value=${item.role}></label>
+        <label>Name : <input id="name" value=${loggedInUser.name} disabled></label>
+            <label>Email : <input id="email" value=${loggedInUser.email} disabled></label>
+            <label>Password : <input type="password" id="password" disabled value=${loggedInUser.role}></label>
         `
         card.className="card";
-    });
+    
     container.appendChild(card);
 
 }
