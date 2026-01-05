9f215760aa9f09a2870c403d55a0a51f84f18d03
Arpita <arpitaambalkar26@gmail.com>
2026-01-05 12:02:45 +0530

    Profile Viewer Page

---
+++ pages/JS/profile_viewer.js
@@ -0,0 +1,37 @@
+/*let loggedInUser=[
+    {
+        name:"Arpita Ambalkar",
+        email:"arpita26@gmail.com",
+        role:"user",
+
+    }
+];*/
+
+const userLoggedKey="loggedInUser";
+localStorage.setItem(userLoggedKey,JSON.stringify(obj));
+let loggedInUser=Array(JSON.parse(localStorage.getItem(userLoggedKey))) || [];
+
+let container=document.getElementById("container");
+let msg=document.getElementById("msg");
+
+function renderloggedUser()
+{
+    if(loggedInUser.length==0)
+    {
+        msg.textContent="No Logged In User";
+        
+    }
+    let card=document.createElement("div");
+    card.innerHTML="";
+    loggedInUser.forEach((item)=>{
+        card.innerHTML=`
+        <label>Name : <input id="name" value=${item.name} disabled></label>
+            <label>Email : <input id="email" value=${item.email} disabled></label>
+            <label>Password : <input type="password" id="password" disabled value=${item.role}></label>
+        `
+        card.className="card";
+    });
+    container.appendChild(card);
+
+}
+renderloggedUser();
